fc82727639ff909580faec3f135508aaade4a40b
Reinger <dvbolotov@edu.hse.ru>
2018-09-23 20:28:06 +0500

    Добавление

--- PublishingHouse/PublishingHouse/Controllers/AddNewObjectController.cs
+++ PublishingHouse/PublishingHouse/Controllers/AddNewObjectController.cs
@@ -8,11 +8,129 @@ namespace PublishingHouse.Controllers
 {
     public class AddNewObjectController : Controller
     {
-        // GET: AddNewObject
-        public ActionResult AddBookExemplar()
+        #region Serie
+        public ActionResult NewSerie()
         {
             return View();
         }
+
+        [HttpPost]
+        public RedirectResult AddSerie(string Name, string Type)
+        {
+            ModelContainer model = new ModelContainer();
+
+            if (Name != "" && Type != "")
+            {
+                Series serie = new Series()
+                {
+                    Name = Name,
+                    CoverType = Type
+                };
+
+                model.SeriesSet.Add(serie);
+                model.SaveChanges();
+
+                return Redirect("~/AddNewObject/NewBookExemplar/" + book.Id);
+            }
+
+            return Redirect("~/AddNewObject/NewSerie");
+        }
+        #endregion
+
+        #region City
+        public ActionResult NewCity()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult AddCity(string Name)
+        {
+            ModelContainer model = new ModelContainer();
+
+            if (Name != "")
+            {
+                City city = new City()
+                {
+                    Name = Name
+                };
+
+                model.CitySet.Add(city);
+                model.SaveChanges();
+
+                return Redirect("~/AddNewObject/NewStore");
+            }
+
+            return Redirect("~/AddNewObject/NewCity");
+        }
+        #endregion
+
+        #region Store
+        public ActionResult NewStore()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult AddStore(string Name, int CityId, string Street, string House)
+        {
+            ModelContainer model = new ModelContainer();
+
+            if (Name != "" && Street != "" && House != "")
+            {
+                Store store = new Store()
+                {
+                    Name = Name,
+                    City = model.CitySet.Find(CityId),
+                    Address = Street+" - "+House
+                };
+
+                model.StoreSet.Add(store);
+                model.SaveChanges();
+
+                return Redirect("~/AddNewObject/NewBookExemplar/" + book.Id);
+            }
+
+            return Redirect("~/AddNewObject/NewStore");
+        }
+        #endregion
+
+        #region BookExemplar
+        public static Books book=new Books();
+
+        public ActionResult NewBookExemplar(int id)
+        {
+            ModelContainer model=new ModelContainer();
+            book = model.BooksSet.Find(id);
+            ViewBag.Book = book;
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult AddBookExemplar(string Year, int StoreId, string Number, int SerieId)
+        {
+            ModelContainer model = new ModelContainer();
+
+            if (Year != "" && Number != "")
+            {
+                BookExemplar book = new BookExemplar()
+                {
+                    Books = AddNewObjectController.book,
+                    Year = Int32.Parse(Year),
+                    Number = Int32.Parse(Number),
+                    Store = model.StoreSet.Find(StoreId),
+                    Series = model.SeriesSet.Find(SerieId)
+                };
+
+                model.BookExemplarSet.Add(book);
+                model.SaveChanges();
+
+                return Redirect("~/Show/ShowBookExemplar/"+book.Id);
+            }
+
+            return Redirect("~/AddNewObject/NewBookExemplar/" + book.Id);
+        }
+        #endregion
 
         #region Book
         public ActionResult NewBook()
@@ -27,7 +145,7 @@ namespace PublishingHouse.Controllers
 
             if (Name != "" && Info != "")
             {
-                Books book = new Books()
+                Books b = new Books()
                 {
                     Name = Name,
                     Description = Info,
@@ -35,7 +153,7 @@ namespace PublishingHouse.Controllers
                     Genre = model.GenreSet.Find(GenreId)
                 };
 
-                model.BooksSet.Add(book);
+                model.BooksSet.Add(b);
                 model.SaveChanges();
 
                 return Redirect("~/Show/ShowBooks");
